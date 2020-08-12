3f8e879dccbef811423eada32c6ff6675c3658a8
adeelafzal <adeelcheema990@gmail.com>
2020-08-12 10:53:29 +0500

    all updates

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -8,6 +8,7 @@ class TaskData extends ChangeNotifier {
     Task(name: 'Buy milk'),
     Task(name: 'Buy eggs'),
     Task(name: 'Buy bread'),
+    Task(name: 'Go to gym'),
   ];
 
   UnmodifiableListView<Task> get tasks {
